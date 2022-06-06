73de3ba91175b41cf1bc322ab3d8ea5ea8f8cc43
mes3n <skidmackan@gmail.com>
2022-06-06 18:54:18 +0200

    added namespace

--- src/logic.cpp
+++ src/logic.cpp
@@ -1,17 +1,17 @@
 #include "logic.hpp"
 
-float distance (sf::Vector2f p1, sf::Vector2f p2) {  // distance formula to get distance between p1 and p2
+float LOGIC::distance (sf::Vector2f p1, sf::Vector2f p2) {  // distance formula to get distance between p1 and p2
     return sqrt((p1.x - p2.x)*(p1.x - p2.x) + (p1.y - p2.y)*(p1.y - p2.y));
 }
 
-sf::Vector2f rotate (sf::Vector2f vec, float angle) {  // rotate a vector by angle radians
+sf::Vector2f LOGIC::rotate (sf::Vector2f vec, float angle) {  // rotate a vector by angle radians
     return sf::Vector2f(
         vec.x * cos(angle) - vec.y * sin(angle),
         vec.x * sin(angle) + vec.y * cos(angle)
     );
 }
 
-sf::Vector2f standardise (sf::Vector2f vec) {  // standardise vector to absolute value of 1
+sf::Vector2f LOGIC::standardise (sf::Vector2f vec) {  // standardise vector to absolute value of 1
     float abs = distance(sf::Vector2f(0, 0), vec);
     return sf::Vector2f(
         vec.x/abs, vec.y/abs
--- src/logic.hpp
+++ src/logic.hpp
@@ -4,8 +4,12 @@
 #include <SFML/System/Vector2.hpp>
 #include <math.h>
 
-float distance (sf::Vector2f, sf::Vector2f);
-sf::Vector2f rotate (sf::Vector2f, float);
-sf::Vector2f standardise (sf::Vector2f);
+namespace LOGIC {
+
+    float distance (sf::Vector2f, sf::Vector2f);
+    sf::Vector2f rotate (sf::Vector2f, float);
+    sf::Vector2f standardise (sf::Vector2f);
+
+}
 
 #endif  // LOGIC_H
